09afe04233bf6b2dc28bff7f839d2465a00639c0
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-10 04:19:47 +0545

    database connection

--- jdbcDBConnect.java
+++ jdbcDBConnect.java
@@ -1,5 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class jdbcDBConnect {
     public static void main(String[] args) {
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","");
+            System.out.println(con);
+        }catch (Exception e){
+            System.out.println("Database not found!!");
+        }
 
     }
 }
